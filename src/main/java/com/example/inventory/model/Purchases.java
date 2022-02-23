4bfe3ca697d4149392f5c0661a325b9bf500b6f7
jeremy <jeremywalker1216@gmail.com>
2022-02-23 18:00:59 -0500

    completed purchase and ingredient models

--- src/main/java/com/example/inventory/model/Purchases.java
+++ src/main/java/com/example/inventory/model/Purchases.java
@@ -1,4 +1,38 @@
 package com.example.inventory.model;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "purchases")
 public class Purchases {
+    
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    public Purchases() {
+    }
+
+    public Purchases(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Purchases{" +
+                "id=" + id +
+                '}';
+    }
 }
